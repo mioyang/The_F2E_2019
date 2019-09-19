25b43641120351226ba3b625b3da52d6630b5ab3
小儀 <pinfudesign@gmail.com>
2019-09-20 02:04:47 +0800

    Ver_1.0.0_Vue.實作header_轉換成動態資料

--- 006/js/all.js
+++ 006/js/all.js
@@ -1,9 +1,17 @@
 var app = new Vue({
     el: '#main',
     data: {
-        message: "hello mio!",
         flexCenter: ['d-flex', 'justify-content-center', 'align-items-center'],
         sectionMinheight: { minHeight: '100vh' },
+        headerSetion: {
+            headerImg: {
+                bannerBg: '/static/images/bg_01.png',
+                siteLogo: '/static/images/logo.svg',
+            },
+            siteTitle: "I don't want to work",
+            logoTxt: '<span>不 想</span> <span>上 班</span>',
+        }
+
     },
 });
 
