249b0fbf90556ffba14de341e59be447e2849ec0
小儀 <pinfudesign@gmail.com>
2019-09-20 00:45:55 +0800

    Ver_1.0.0_Vue.實作header

---
+++ 006/js/all.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#main',
+    data: {
+        message: "hello mio!",
+        flexCenter: ['d-flex', 'justify-content-center', 'align-items-center'],
+        sectionMinheight: { minHeight: '100vh' },
+    },
+});
+
