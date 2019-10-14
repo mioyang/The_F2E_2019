5bc52611f555059e34a64fe4bf7282c140a2b272
小儀 <pinfudesign@gmail.com>
2019-10-15 02:04:26 +0800

    fix

--- 006/js/all.js
+++ 006/js/all.js
@@ -7,18 +7,46 @@ const headers = {
     'Authorization': 'Bearer CGgGz2y5DZ3xFQ5ubOt5S268uvKONejWTDZn5v2ISewAWaGdGWsA2vq07pPQ'
 }
 
+
+Vue.component('card-rooms', {
+    props: ['roomLists'],
+    template: '#cardElement',
+});
+
+Vue.component("footer-component", {
+    props: ['siteLogo', 'sitePhone', 'siteAdd', 'siteIg'],
+    template:
+        `<div id="footer" class="row justify-content-end">
+            <div class="col-12 col-xl-4 d-flex justify-content-end align-items-center p-3 mr-5">
+                <div class="f-logo pr-5 mr-5"><img :src="siteLogo" alt="" srcset=""></div>
+                <ul>
+                    <li><i class="fas fa-phone"></i>{{sitePhone}}</li>
+                    <li><i class="fas fa-map-marker-alt"></i>{{siteAdd}}</li>
+                    <li><i class="fab fa-instagram"></i>{{siteIg}}</li>
+                </ul>
+            </div>
+        </div>`,
+});
+
+
 var app = new Vue({
     el: '#main',
     data: {
+        show: true,
         flexCenter: ['d-flex', 'justify-content-center', 'align-items-center'],
         sectionMinheight: { minHeight: '100vh' },
-        headerSetion: {
-            headerImg: {
-                bannerBg: '/static/images/bg_01.png',
-                siteLogo: '/static/images/logo.svg',
+        siteInfos: {
+            siteImgs: {
+                bannerBg: "/static/images/bg_01.png",
+                siteLogo: "/static/images/logo.svg",
+                footerLogo: "/static/images/logo_white.svg",
+                scrollBtn: "/static/images/slide_btn.svg",
             },
+            siteName: "不 想 │ 上 班",
             siteTitle: "I don't want to work",
-            logoTxt: '<span>不 想</span> <span>上 班</span>',
+            sitePhone: "07-536-0000",
+            siteAdd: "香港銅鑼灣興發街1號",
+            siteIg: "I_dont_wank_to_work",
         },
         rooms: [],
         Singleroom: [],
@@ -41,6 +69,12 @@ var app = new Vue({
         //         console.log('連線異常');
         //     });
     },
+    methods: {
+        // scrollDown: function () {
+        //     let event = window.addEventListener('scroll', e => console.log(e.target), true);
+        //     console.log(event);
+        // },
+    },
 });
 
 
@@ -55,5 +89,16 @@ let siteAnimate = ScrollReveal({
 ScrollReveal().reveal('.logo', { duration: 2000, origin: 'left', delay: 325 });
 ScrollReveal().reveal('.title', { duration: 2000, origin: 'right', delay: 500 });
 
-
-
+Vue.use(VueScrollTo, {
+    container: "body",
+    duration: 1200,
+    easing: "linear",
+    offset: 0,
+    force: true,
+    cancelable: true,
+    onStart: false,
+    onDone: false,
+    onCancel: false,
+    x: false,
+    y: true
+});
